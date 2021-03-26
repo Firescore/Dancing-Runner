74a80322a4552ffd40aa709c542ac63c801f002b
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-03-26 15:23:49 +0530

    updatte

---
+++ Assets/Script/_Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Enemy : MonoBehaviour
+{
+    public float _Speed = 5;
+    public Animator maleAnimation;
+    public Animator femaleAnimation;
+
+    public Transform male, female;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * _Speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "1":
+                maleAnimation.SetTrigger("limboo");
+                femaleAnimation.SetTrigger("limboo");
+                break;
+
+            case "2":
+                maleAnimation.SetTrigger("waltz");
+                femaleAnimation.SetTrigger("waltz");
+                _Speed = 1.2f;
+                break;
+        }
+    }
+}
---
+++ Assets/Script/_UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _UIManager : MonoBehaviour
+{
+    public _Player player;
+
+    bool isLinbooPressed = false;
+    bool isWaltzPressed = false;
+    void Start()
+    {
+        player = FindObjectOfType<_Player>();
+    }
+
+
+    private void Update()
+    {
+        
+        if(isLinbooPressed && !isWaltzPressed)
+            player.female.localPosition = Vector3.Lerp(player.female.localPosition, new Vector3(1f, player.female.localPosition.y, player.female.localPosition.z), 1f * Time.deltaTime);
+
+        if(isWaltzPressed && !isLinbooPressed)
+            player.female.localPosition = new Vector3(0.54f, player.female.localPosition.y, player.female.localPosition.z);
+    }
+    public void setLimbooDance()
+    {
+        //player.female.localPosition = new Vector3(0.73f, player.female.localPosition.y, player.female.localPosition.z);
+        
+        isLinbooPressed = true;
+        isWaltzPressed = false;
+        player.maleAnimation.SetTrigger("limboo");
+        player.femaleAnimation.SetTrigger("limboo");
+    }
+
+    public void setWaltzDance()
+    {
+
+        isLinbooPressed = false;
+        isWaltzPressed = true;
+        player.maleAnimation.SetTrigger("waltz");
+        player.femaleAnimation.SetTrigger("waltz");
+    }
+
+    public void setBboyDance()
+    {
+        isLinbooPressed = true;
+        isWaltzPressed = false;
+        player.maleAnimation.SetTrigger("bboy");
+        player.femaleAnimation.SetTrigger("bboy");
+    }
+}
